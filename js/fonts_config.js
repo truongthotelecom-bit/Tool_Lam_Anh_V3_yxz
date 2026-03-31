57a50be15ed4ddc1566e3bfeaa918a558abf5d7e
truongthotelecom-bit <truongthotelecom@gmail.com>
2026-03-31 10:19:46 +0700

    Add files via upload

---
+++ js/fonts_config.js
@@ -0,0 +1,33 @@
+/**
+ * DANH SÁCH FONT HỆ THỐNG MẶC ĐỊNH
+ * Bạn có thể thêm tên font mới vào đây để chúng xuất hiện trong tất cả các ô chọn font.
+ * Lưu ý: Tên font phải trùng với tên font đã được nạp qua Google Fonts hoặc FontFace.
+ */
+window.sysFonts = [
+    'Arial', 
+    'Verdana', 
+    'Tahoma', 
+    'Times New Roman', 
+    'Courier New', 
+    'Segoe UI', 
+    'Impact',
+    'Roboto', 
+    'Montserrat', 
+    'Oswald', 
+    'Dancing Script', 
+    'Pattaya', 
+    'Inter', 
+    'Outfit',
+    'Playfair Display', 
+    'Bungee', 
+    'Lobster', 
+    'Pacifico', 
+    'Saira Stencil One', 
+    'Charm',
+    'Sedgwick Ave Display', 
+    'Century', 
+    'Century-Bold', 
+    'UVN Saigon', 
+    'UVN Dam Cuoi', 
+    'HL Thu phap'
+];
